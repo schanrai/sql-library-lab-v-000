a9dd532523980d40a0d8ba71bd606600bc3090b6
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 17:04:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,6 +35,7 @@ def select_series_title_with_most_human_characters
   JOIN characters ON characters.series_id = series.id
   WHERE characters.species = 'human'
   GROUP BY series.title
+  ORDER BY COUNT(*) DESC
   LIMIT 1;"
 end
 
